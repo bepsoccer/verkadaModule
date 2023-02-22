2742e04ae9507923ca2ed6e703f0c9fd82d847fb
Brad Parker <brad@parker1723.com>
2023-02-22 15:18:27 -0600

    tweak for token submission

--- Public/Get-VerkadaAccessUser.ps1
+++ Public/Get-VerkadaAccessUser.ps1
@@ -18,19 +18,25 @@ function Get-VerkadaAccessUser
 		[Parameter(ValueFromPipelineByPropertyName = $true, Position = 0)]
 		[String]$org_id = $Global:verkadaConnection.org_id,
 		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 1)]
-		[String]$userId
+		[String]$userId,
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$x_verkada_token = $Global:verkadaConnection.csrfToken,
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$x_verkada_auth = $Global:verkadaConnection.userToken
 
 	)
 
 	Begin {
-		if (!($org_id)){Write-Warning 'Missing org_id which is required'; return}
-		if (!($Global:verkadaConnection)){Write-Warning 'Missing auth token which is required'; return}
-		if ($Global:verkadaConnection.authType -ne 'UnPwd'){Write-Warning 'Un/Pwd auth is required'; return}
+		#if (!($org_id)){Write-Warning 'Missing org_id which is required'; return}
+		#if (!($Global:verkadaConnection)){Write-Warning 'Missing auth token which is required'; return}
+		#if ($Global:verkadaConnection.authType -ne 'UnPwd'){Write-Warning 'Un/Pwd auth is required'; return}
 	} #end begin
 	
 	Process {
 		$url = "https://vcerberus.command.verkada.com/user/$org_id/$userId"
 
-		Invoke-VerkadaRestMethod $url $org_id -UnPwd
+		Invoke-VerkadaRestMethod $url $org_id -x_verkada_token $x_verkada_token -x_verkada_auth $x_verkada_auth -UnPwd
 	} #end process
 } #end function
