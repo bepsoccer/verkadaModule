4411d88f7d70b564aa4174b519138a4f523972cc
Brad Parker <brad@parker1723.com>
2023-02-22 15:18:06 -0600

    tweak for including badge with Access user creation

--- Public/Add-VerkadaAccessUser.ps1
+++ Public/Add-VerkadaAccessUser.ps1
@@ -36,6 +36,16 @@ function Add-VerkadaAccessUser
 		[datetime]$expiration,
 		[Parameter(ValueFromPipelineByPropertyName = $true)]
 		[bool]$sendInviteEmail=$false,
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[String]$cardType,
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[String]$cardNumber,
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[String]$cardNumberHex,
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[String]$facilityCode,
+		[Parameter()]
+		[Switch]$includeBadge,
 		[Parameter()]
 		[int]$threads=$null
 
@@ -72,11 +82,53 @@ function Add-VerkadaAccessUser
 			} until ($job -le $threads)
 
 			Start-Job -Name $email -InitializationScript {Import-Module verkadaModule.psm1} -ScriptBlock {
-				Invoke-VerkadaFormCall $using:url $using:org_id $using:form_params -x_verkada_token $using:x_verkada_token -x_verkada_auth $using:x_verkada_auth
+				$output = Invoke-VerkadaFormCall $using:url $using:org_id $using:form_params -x_verkada_token $using:x_verkada_token -x_verkada_auth $using:x_verkada_auth
+				$res = @{}
+				$res.created = (Get-Date -Date "01-01-1970") + ([System.TimeSpan]::FromSeconds(($output.users.created)))
+				$res.userId = $output.users.userId
+				$res.firstName = $output.users.firstName
+				$res.lastName = $output.users.lastName
+				$res.email = $output.users.email
+
+				$response = $res | ConvertTo-Json -Depth 100 | ConvertFrom-Json
+				
+				if ($using:includeBadge.IsPresent){ 
+					$eval = "`$response | Add-VerkadaAccessBadgeToUser -org_id $using:org_id -x_verkada_token $using:x_verkada_token -x_verkada_auth $using:x_verkada_auth -cardType $using:cardType"
+					if (!([string]::IsNullOrEmpty($using:cardNumber))){$eval +=" -cardNumber $using:cardNumber"}
+					if (!([string]::IsNullOrEmpty($using:cardNumberHex))){$eval +=" -cardNumberHex $using:cardNumberHex"}
+					if (!([string]::IsNullOrEmpty($using:facilityCode))){$eval +=" -facilityCode $using:facilityCode"}
+	
+					$output2 = invoke-expression $eval
+					$res.accessCards = $output2.accessCards
+				}
+
+				$response = $res | ConvertTo-Json -Depth 100 | ConvertFrom-Json
+				$response
 			} | Out-Null
 			Get-Job -State Completed | Receive-Job -AutoRemoveJob -Wait
 		} else {
-			Invoke-VerkadaFormCall $url $org_id $form_params -x_verkada_token $x_verkada_token -x_verkada_auth $x_verkada_auth
+			$output = Invoke-VerkadaFormCall $url $org_id $form_params -x_verkada_token $x_verkada_token -x_verkada_auth $x_verkada_auth
+			$res = @{}
+			$res.created = (Get-Date -Date "01-01-1970") + ([System.TimeSpan]::FromSeconds(($output.users.created)))
+			$res.userId = $output.users.userId
+			$res.firstName = $output.users.firstName
+			$res.lastName = $output.users.lastName
+			$res.email = $output.users.email
+
+			$response = $res | ConvertTo-Json -Depth 100 | ConvertFrom-Json
+			
+			if ($includeBadge.IsPresent){ 
+				$eval = "`$response | Add-VerkadaAccessBadgeToUser -cardType $cardType"
+				if (!([string]::IsNullOrEmpty($cardNumber))){$eval +=" -cardNumber $cardNumber"}
+				if (!([string]::IsNullOrEmpty($cardNumberHex))){$eval +=" -cardNumberHex $cardNumberHex"}
+				if (!([string]::IsNullOrEmpty($facilityCode))){$eval +=" -facilityCode $facilityCode"}
+
+				$output2 = invoke-expression $eval
+				$res.accessCards = $output2.accessCards
+			}
+			$response = $res | ConvertTo-Json -Depth 100 | ConvertFrom-Json
+			$response
+
 		}
 
 	} #end process
