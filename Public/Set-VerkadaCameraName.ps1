958d67dab61c3037eb1d684cccf47e19edb51bbf
Brad Parker <brad@parker1723.com>
2023-03-02 12:42:21 -0600

    Add Set-VerkadaCameName
    Add Set-VerkadaCameName

---
+++ Public/Set-VerkadaCameraName.ps1
@@ -0,0 +1,58 @@
+function Set-VerkadaCameraName
+{
+	<#
+		.SYNOPSIS
+		Set the name of a camera in an organization
+		.DESCRIPTION
+
+		.NOTES
+
+		.EXAMPLE
+
+		.LINK
+
+	#>
+
+	[CmdletBinding(PositionalBinding = $true, DefaultParameterSetName = 'email')]
+	Param(
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[ValidateNotNullOrEmpty()]
+		[String]$org_id = $Global:verkadaConnection.org_id,
+		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+		[Alias("cameraId")]
+		[String]$camera_id,
+		[Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+		[Alias("name")]
+		[String]$camera_name,
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$x_verkada_token = $Global:verkadaConnection.csrfToken,
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$x_verkada_auth = $Global:verkadaConnection.userToken
+	)
+
+	Begin {
+		#if (!($org_id)){Write-Warning 'Missing org_id which is required'; return}
+		#if (!($Global:verkadaConnection)){Write-Warning 'Missing auth token which is required'; return}
+		#if ($Global:verkadaConnection.authType -ne 'UnPwd'){Write-Warning 'Un/Pwd auth is required'; return}
+
+		$url = "https://vprovision.command.verkada.com/camera/name/set"
+		$response = @()
+	} #end begin
+	
+	Process {
+		$body_params = @{
+			"cameraId"				= $camera_id
+			"name"						= $camera_name
+		}
+		
+		$res = Invoke-VerkadaRestMethod $url $org_id $body_params -x_verkada_token $x_verkada_token -x_verkada_auth $x_verkada_auth -Method 'POST' -UnPwd
+		$response += $res.cameras
+
+	} #end process
+
+	End {
+		return $response
+	}
+} #end function
