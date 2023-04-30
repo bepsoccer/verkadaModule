83d6c40aa207311ac2d495a3c3df5831ef4ec3f7
Brad Parker <brad@parker1723.com>
2023-04-30 16:38:53 -0500

    update

--- verkadaModule/Public/Get-VerkadaAccessUser.ps1
+++ verkadaModule/Public/Get-VerkadaAccessUser.ps1
@@ -39,7 +39,12 @@ function Get-VerkadaAccessUser
 		#The Verkada Auth(session auth) token of the user running the command
 		[Parameter()]
 		[ValidateNotNullOrEmpty()]
-		[string]$x_verkada_auth = $Global:verkadaConnection.userToken
+		[string]$x_verkada_auth = $Global:verkadaConnection.userToken,
+		#The UUID of the user account making the request
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[string]$usr = $Global:verkadaConnection.usr
 	)
 
 	Begin {
@@ -47,10 +52,147 @@ function Get-VerkadaAccessUser
 		if ([string]::IsNullOrEmpty($org_id)) {throw "org_id is missing but is required!"}
 		if ([string]::IsNullOrEmpty($x_verkada_token)) {throw "x_verkada_token is missing but is required!"}
 		if ([string]::IsNullOrEmpty($x_verkada_auth)) {throw "x_verkada_auth is missing but is required!"}
+		if ([string]::IsNullOrEmpty($usr)) {throw "usr_id is missing but is required!"}
+		
+		$url = "https://vgateway.command.verkada.com/graphql"
+
+		$queryBase = 'query GetAccessUserProfile($id: ID) {
+	user(id: $id) {
+		...AccessUserProfile
+		__typename
+	}
+}'
+
+		$AccessUserProfileFragment = 'fragment AccessUserProfile on User {
+	...AccessUser
+	accessCardsRaw {
+		cacheId
+		organizationId
+		userId
+		active
+		cardId
+		cardType
+		lastUsed
+		modified
+		cardParams {
+			cardNumber
+			cardNumberHex
+			facilityCode
+			__typename
+		}
+		__typename
+	}
+	userCodesRaw {
+		code
+		lastUsed
+		__typename
+	}
+	__typename
+}'
+
+		$AccessUserFragment = 'fragment AccessUser on User {
+	...AccessUserBasic
+	employeeId
+	employeeTitle
+	department
+	departmentId
+	companyName
+	employeeType
+	mobileAccess
+	bluetoothAccess
+	accessUserRoles {
+		role
+		__typename
+	}
+	accessGroups {
+		group {
+			userGroupId
+			name
+			__typename
+		}
+		added
+		__typename
+	}
+	accessCards {
+		active
+		cardId
+		cardType
+		lastUsed
+		modified
+		cardParams {
+			cardNumber
+			facilityCode
+			__typename
+		}
+		__typename
+	}
+	userCodes {
+		code
+		lastUsed
+		__typename
+	}
+	roleGrant: roleGrants(filter: {includeExpired: true}) {
+		granteeId
+		grantId
+		entityId
+		realGranteeId
+		start
+		expiration
+		role {
+			key
+			name
+			roleId
+			permissions {
+				permission
+				permissionId
+				__typename
+			}
+			__typename
+		}
+		__typename
+	}
+	__typename
+}'
+
+		$AccessUserBasicFragment = 'fragment AccessUserBasic on User {
+	userId
+	name
+	phone
+	firstName
+	middleName
+	lastName
+	email
+	emailVerified
+	organizationId
+	created
+	modified
+	provisioned
+	lastLogin
+	lastActiveAccess
+	deactivated
+	deleted
+	roleGrant: roleGrants(filter: {includeExpired: true}) {
+		granteeId
+		grantId
+		entityId
+		organizationId
+		realGranteeId
+		roleId
+		start
+		expiration
+		__typename
+	}
+	__typename
+}'
+
+		$query = $queryBase + "`n" + $AccessUserProfileFragment + "`n" + $AccessUserFragment + "`n" + $AccessUserBasicFragment
 	} #end begin
 	
 	Process {
-		$url = "https://vcerberus.command.verkada.com/user/$org_id/$userId"
-		Invoke-VerkadaRestMethod $url $org_id -x_verkada_token $x_verkada_token -x_verkada_auth $x_verkada_auth -UnPwd
+		$variables = '{"id":""}' | ConvertFrom-Json
+		$variables.id = $userId
+
+		$user = Invoke-VerkadaGraphqlCall $url -query $query -qlVariables $variables -org_id $org_id -method 'Post' -propertyName 'user' -x_verkada_token $x_verkada_token -x_verkada_auth $x_verkada_auth -usr $usr
+		return $user
 	} #end process
 } #end function
