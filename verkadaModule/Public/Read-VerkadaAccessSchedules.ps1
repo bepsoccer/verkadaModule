508c5236f7c5da3e8e1e8abd6a4b4f75202efbc4
Brad Parker <brad@parker1723.com>
2023-04-30 23:01:19 -0500

    add functions

---
+++ verkadaModule/Public/Read-VerkadaAccessSchedules.ps1
@@ -0,0 +1,69 @@
+function Read-VerkadaAccessSchedules{
+	<#
+		.SYNOPSIS
+		Gathers all Command Access schedules in an organization
+
+		.DESCRIPTION
+		This function will return all the Access schedules in an organization with "User" schedules being Access Levels and "Door" being Door Schedules.
+
+		.LINK
+		https://github.com/bepsoccer/verkadaModule/blob/master/docs/function-documentation/Read-VerkadaAccessSchedules.md
+
+		.EXAMPLE
+		Read-VerkadaAccessSchedules.	The org_id and tokens will be populated from the cached created by Connect-Verkada.
+
+		.EXAMPLE
+		Read-VerkadaAccessSchedules -org_id 'deds343-uuid-of-org' -x_verkada_token 'sd78ds-uuid-of-verkada-token' -x_verkada_auth 'auth-token-uuid-dscsdc'.	The org_id and tokens are submitted as parameters in the call.
+	#>
+	[CmdletBinding(PositionalBinding = $true)]
+	param (
+		#The UUID of the organization the user belongs to
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[String]$org_id = $Global:verkadaConnection.org_id,
+		#The Verkada(CSRF) token of the user running the command
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[string]$x_verkada_token = $Global:verkadaConnection.csrfToken,
+		#The Verkada Auth(session auth) token of the user running the command
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$x_verkada_auth = $Global:verkadaConnection.userToken,
+		#The UUID of the user account making the request
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[string]$usr = $Global:verkadaConnection.usr
+	)
+	
+	begin {
+		#parameter validation
+		if ([string]::IsNullOrEmpty($org_id)) {throw "org_id is missing but is required!"}
+		if ([string]::IsNullOrEmpty($x_verkada_token)) {throw "x_verkada_token is missing but is required!"}
+		if ([string]::IsNullOrEmpty($x_verkada_auth)) {throw "x_verkada_auth is missing but is required!"}
+		if ([string]::IsNullOrEmpty($usr)) {throw "usr is missing but is required!"}
+
+		$url = "https://vcerberus.command.verkada.com/organizations/$org_id/schedules"
+		$body = ""
+	}
+	
+	process {
+		try {
+			$response = Invoke-VerkadaCommandCall $url $org_id $body -x_verkada_token $x_verkada_token -x_verkada_auth $x_verkada_auth -usr $usr -Method 'GET' | Select-Object -ExpandProperty schedules
+		}
+		catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+			$err = $_.ErrorDetails | ConvertFrom-Json
+			$errorMes = $_ | Convertto-Json -WarningAction SilentlyContinue
+			$err | Add-Member -NotePropertyName StatusCode -NotePropertyValue (($errorMes | ConvertFrom-Json -Depth 100 -WarningAction SilentlyContinue).Exception.Response.StatusCode) -Force
+
+			Write-Host "$($err.StatusCode) - $($err.message)" -ForegroundColor Red
+			Return
+		}
+	}
+	
+	end {
+		return $response
+	}
+}
