aaca04e70caf31bf1ad90906bab5b0a6ba7ee108
Brad Parker <brad@parker1723.com>
2023-05-01 15:03:17 -0500

    fix

--- verkadaModule/Public/Get-VerkadaAccessGroup.ps1
+++ verkadaModule/Public/Get-VerkadaAccessGroup.ps1
@@ -43,12 +43,7 @@ function Get-VerkadaAccessGroup{
 		#The Verkada Auth(session auth) token of the user running the command
 		[Parameter()]
 		[ValidateNotNullOrEmpty()]
-		[string]$x_verkada_auth = $Global:verkadaConnection.userToken,
-		#The UUID of the user account making the request
-		[Parameter()]
-		[ValidateNotNullOrEmpty()]
-		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
-		[string]$usr = $Global:verkadaConnection.usr
+		[string]$x_verkada_auth = $Global:verkadaConnection.userToken
 	)
 	
 	begin {
@@ -56,7 +51,6 @@ function Get-VerkadaAccessGroup{
 		if ([string]::IsNullOrEmpty($org_id)) {throw "org_id is missing but is required!"}
 		if ([string]::IsNullOrEmpty($x_verkada_token)) {throw "x_verkada_token is missing but is required!"}
 		if ([string]::IsNullOrEmpty($x_verkada_auth)) {throw "x_verkada_auth is missing but is required!"}
-		if ([string]::IsNullOrEmpty($usr)) {throw "usr is missing but is required!"}
 
 		$url = 'https://vcerberus.command.verkada.com/user_groups/get'
 
