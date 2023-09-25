c531f5300b38a4b3d72e12f5f2336c3bf51d8646
Brad Parker <brad@parker1723.com>
2023-09-24 21:50:12 -0500

    reorder params

--- verkadaModule/Public/Add-VerkadaCommandUser.ps1
+++ verkadaModule/Public/Add-VerkadaCommandUser.ps1
@@ -82,6 +82,15 @@ function Add-VerkadaCommandUser{
 		[Parameter(ValueFromPipelineByPropertyName = $true)]
 		[ValidatePattern("^\+?[1-9]\d{1,14}$")]
 		[String]$phone,
+		#The UUID of the organization the user belongs to
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[String]$org_id = $Global:verkadaConnection.org_id,
+		#The public API key to be used for calls that hit the public API gateway
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[String]$x_api_key = $Global:verkadaConnection.token,
 		#Switch to write errors to file
 		[Parameter()]
 		[switch]$errorsToFile
