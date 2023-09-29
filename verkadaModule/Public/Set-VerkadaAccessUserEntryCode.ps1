b2146a898ce60518cc4bb4a540332a754026e061
Brad Parker <brad@parker1723.com>
2023-09-29 10:08:28 -0500

    new function
    Set-VerkadaAccessUserEntryCode

---
+++ verkadaModule/Public/Set-VerkadaAccessUserEntryCode.ps1
@@ -0,0 +1,117 @@
+function Set-VerkadaAccessUserEntryCode{
+	<#
+		.SYNOPSIS
+		Sets an entry code for an Access User in an organization using https://apidocs.verkada.com/reference/putaccessuserpinviewv1
+
+		.DESCRIPTION
+		Given the user defined External ID or Verkada defined User ID (but not both), set the entry code for a user. Entry code value will be passed as a parameter in a json payload.Returns the updated Access Information Object.
+		The org_id and reqired token can be directly submitted as parameters, but is much easier to use Connect-Verkada to cache this information ahead of time and for subsequent commands.
+
+		.LINK
+		https://github.com/bepsoccer/verkadaModule/blob/master/docs/function-documentation/Set-VerkadaAccessUserEntryCode.md
+
+		.EXAMPLE
+		Set-VerkadaAccessUserEntryCode -userId '801c9551-b04c-4293-84ad-b0a6aa0588b3' -entryCode '12345'
+		This will set an entry code of 12345 to the user specified.  The org_id and tokens will be populated from the cached created by Connect-Verkada.
+		
+		.EXAMPLE
+		Set-VerkadaAccessUserEntryCode -externalId 'newUserUPN@contoso.com' -entryCode '12345' -x_api_key 'sd78ds-uuid-of-verkada-token'
+		This will set an entry code of 12345 to the user specified.  The org_id and tokens are submitted as parameters in the call.
+	#>
+	[CmdletBinding(PositionalBinding = $true)]
+	[Alias("Set-VrkdaAcUsrEntryCo","st-VrkdaAcUsrEntryCo")]
+	param (
+		#The UUID of the user
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[Alias('user_id')]
+		[String]$userId,
+		#unique identifier managed externally provided by the consumer
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[Alias('external_id')]
+		[String]$externalId,
+		#The Access entry code
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[ValidatePattern('^\d{4,16}$')]
+		[Alias('entry_code')]
+		[string]$entryCode,
+		#The flag that states whether or not the client wants to apply the given entry code to the given user even if the entry code is already in use by another user. This will reset the other user's entry code
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[bool]$override=$false,
+		#The UUID of the organization the user belongs to
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[String]$org_id = $Global:verkadaConnection.org_id,
+		#The public API key to be used for calls that hit the public API gateway
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[String]$x_api_key = $Global:verkadaConnection.token,
+		#Switch to write errors to file
+		[Parameter()]
+		[switch]$errorsToFile
+	)
+	
+	begin {
+		$url = "https://api.verkada.com/access/v1/access_users/user/entry_code"
+		#parameter validation
+		if ([string]::IsNullOrEmpty($org_id)) {throw "org_id is missing but is required!"}
+		if ([string]::IsNullOrEmpty($x_api_key)) {throw "x_api_key is missing but is required!"}
+		$myErrors = @()
+	} #end begin
+	
+	process {
+		if ([string]::IsNullOrEmpty($entryCode)) {
+			Write-Error "startDate is missing but is required!"
+			return
+		}
+		if ([string]::IsNullOrEmpty($externalId) -and [string]::IsNullOrEmpty($userId)){
+			Write-Error "Either externalId or userId required"
+			return
+		}
+
+		$body_params = @{
+			'entry_code'	= $entryCode
+		}
+		
+		$query_params = @{}
+		if (!([string]::IsNullOrEmpty($userId))){
+			$query_params.user_id = $userId
+		} elseif (!([string]::IsNullOrEmpty($externalId))){
+			$query_params.external_id = $externalId
+		}
+		$query_params.override = $override
+		
+		try {
+			$response = Invoke-VerkadaRestMethod $url $org_id $x_api_key $query_params -body_params $body_params -method PUT
+			return $response
+		}
+		catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+			$err = $_.ErrorDetails | ConvertFrom-Json
+			$errorMes = $_ | Convertto-Json -WarningAction SilentlyContinue
+			$err | Add-Member -NotePropertyName StatusCode -NotePropertyValue (($errorMes | ConvertFrom-Json -Depth 100 -WarningAction SilentlyContinue).Exception.Response.StatusCode) -Force
+			$msg = "$($err.StatusCode) - $($err.message)"
+			$msg += ": $(($query_params + $body_params) | ConvertTo-Json -Compress)"
+			Write-Error $msg
+			$myErrors += $msg
+			$msg = $null
+		}
+		catch [VerkadaRestMethodException] {
+			$msg = $_.ToString()
+			$msg += ": $(($query_params + $body_params) | ConvertTo-Json -Compress)"
+			Write-Error $msg
+			$myErrors += $msg
+			$msg = $null
+		}
+	} #end process
+	
+	end {
+		if ($errorsToFile.IsPresent){
+			if (![string]::IsNullOrEmpty($myErrors)){
+				Get-Date | Out-File ./errors.txt -Append
+				$myErrors | Out-File ./errors.txt -Append
+			}
+		}
+	} #end end
+} #end function
