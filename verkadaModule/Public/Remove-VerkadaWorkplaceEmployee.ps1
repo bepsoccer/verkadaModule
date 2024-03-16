559bbf73ceba7e493fb68993e69af4fb316d0f10
Brad Parker <brad@parker1723.com>
2024-03-16 10:44:25 -0500

    initial additions
    Add-VerkadaWorkplaceEmployee
    Get-VerkadaWorkplaceEmployee
    Remove-VerkadaWorkplaceEmployee
    Read-VerkadaWorkplaceEmployees

---
+++ verkadaModule/Public/Remove-VerkadaWorkplaceEmployee.ps1
@@ -0,0 +1,57 @@
+function Remove-VerkadaWorkplaceEmployee{
+	<#
+		.SYNOPSIS
+
+		.DESCRIPTION
+		
+		The org_id and reqired tokens can be directly submitted as parameters, but is much easier to use Connect-Verkada to cache this information ahead of time and for subsequent commands.
+
+		.LINK
+		https://github.com/bepsoccer/verkadaModule/blob/master/docs/function-documentation/Remove-VerkadaWorkplaceEmployee.md
+
+		.EXAMPLE
+		The org_id and tokens will be populated from the cached created by Connect-Verkada.
+
+		.EXAMPLE
+		The org_id and tokens are submitted as parameters in the call.
+	#>
+	[CmdletBinding(PositionalBinding = $true)]
+	[Alias("")]
+	param (
+		#The UUID of the organization the user belongs to
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[String]$org_id = $Global:verkadaConnection.org_id,
+		#The Verkada(CSRF) token of the user running the command
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[string]$x_verkada_token = $Global:verkadaConnection.csrfToken,
+		#The Verkada Auth(session auth) token of the user running the command
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$x_verkada_auth = $Global:verkadaConnection.userToken,
+		#The UUID of the user account making the request
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
+		[string]$usr = $Global:verkadaConnection.usr
+	)
+	
+	begin {
+		#parameter validation
+		if ([string]::IsNullOrEmpty($org_id)) {throw "org_id is missing but is required!"}
+		if ([string]::IsNullOrEmpty($x_verkada_token)) {throw "x_verkada_token is missing but is required!"}
+		if ([string]::IsNullOrEmpty($x_verkada_auth)) {throw "x_verkada_auth is missing but is required!"}
+		if ([string]::IsNullOrEmpty($usr)) {throw "usr is missing but is required!"}
+	} #end begin
+	
+	process {
+		
+	} #end process
+	
+	end {
+		
+	} #end end
+} #end function
