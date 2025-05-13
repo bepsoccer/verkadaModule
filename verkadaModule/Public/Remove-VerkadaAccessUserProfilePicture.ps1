260ef4d1043139231d5c21cdf43b0cc7a0fae317
Brad Parker <brad@parker1723.com>
2025-05-13 18:46:19 -0500

    Updates Remove-VerkadaAccessUserProfilePicture
    Refactors the function to use the public API for removing user profile pictures.
    
    Changes include:
    - Using externalId or userId to identify the user.
    - Adding a region parameter to target different API regions.
    - Switching to using the public API token (x_verkada_auth_api).
    - Adding errorsToFile parameter to allow writing errors to a file.
    
    closes #219

--- verkadaModule/Public/Remove-VerkadaAccessUserProfilePicture.ps1
+++ verkadaModule/Public/Remove-VerkadaAccessUserProfilePicture.ps1
@@ -1,11 +1,11 @@
 function Remove-VerkadaAccessUserProfilePicture{
 	<#
 		.SYNOPSIS
-		Removes an Access user's profile picture in an organization.
+		Deletes a profile photo for the specified user using https://apidocs.verkada.com/reference/deleteprofilephotoviewv1
 
 		.DESCRIPTION
-		This will remove the Access user's, specified by the userId, profile picture.
-		The org_id and reqired tokens can be directly submitted as parameters, but is much easier to use Connect-Verkada to cache this information ahead of time and for subsequent commands.
+		This will remove the Access user's, specified by the user_Id or external_Id, profile picture.
+		The org_id and reqired token can be directly submitted as parameters, but is much easier to use Connect-Verkada to cache this information ahead of time and for subsequent commands.
 
 		.LINK
 		https://github.com/bepsoccer/verkadaModule/blob/master/docs/function-documentation/Remove-VerkadaAccessUserProfilePicture.md
@@ -15,8 +15,8 @@ function Remove-VerkadaAccessUserProfilePicture{
 		This removes the Access user's profile picture with userId 801c9551-b04c-4293-84ad-b0a6aa0588b3.  The org_id and tokens will be populated from the cached created by Connect-Verkada.
 
 		.EXAMPLE
-		Remove-VerkadaAccessUserProfilePicture -userId '801c9551-b04c-4293-84ad-b0a6aa0588b3' -x_verkada_token 'a366ef47-2c20-4d35-a90a-10fd2aee113a' -x_verkada_auth 'auth-token-uuid-dscsdc' -usr 'a099bfe6-34ff-4976-9d53-ac68342d2b60'
-		This removes the Access user's profile picture with userId 801c9551-b04c-4293-84ad-b0a6aa0588b3.  The org_id and tokens are submitted as parameters in the call.
+		Remove-VerkadaAccessUserProfilePicture -externalId 'newUserUPN@contoso.com' -org_id '7cd47706-f51b-4419-8675-3b9f0ce7c12d' -x_verkada_auth_api 'sd78ds-uuid-of-verkada-token'
+		This removes the Access user's profile picture with externalId newUserUPN@contoso.com.  The org_id and tokens are submitted as parameters in the call.
 	#>
 	[CmdletBinding(PositionalBinding = $true)]
 	[Alias("Remove-VrkdaAcUsrPrflPic","rm-VrkdaAcUsrPrflPic")]
@@ -27,65 +27,79 @@ function Remove-VerkadaAccessUserProfilePicture{
 		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
 		[Alias('user_id')]
 		[String]$userId,
+		#unique identifier managed externally provided by the consumer
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[Alias('external_id')]
+		[String]$externalId,
 		#The UUID of the organization the user belongs to
 		[Parameter(ValueFromPipelineByPropertyName = $true)]
 		[ValidateNotNullOrEmpty()]
 		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
 		[String]$org_id = $Global:verkadaConnection.org_id,
-		#The Verkada(CSRF) token of the user running the command
+		#The public API token obatined via the Login endpoint to be used for calls that hit the public API gateway
 		[Parameter()]
 		[ValidateNotNullOrEmpty()]
-		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
-		[string]$x_verkada_token = $Global:verkadaConnection.csrfToken,
-		#The Verkada Auth(session auth) token of the user running the command
+		[String]$x_verkada_auth_api = $Global:verkadaConnection.x_verkada_auth_api,
+		#The region of the public API to be used
 		[Parameter()]
-		[ValidateNotNullOrEmpty()]
-		[string]$x_verkada_auth = $Global:verkadaConnection.userToken,
-		#The UUID of the user account making the request
+		[ValidateSet('api','api.eu','api.au')]
+		[String]$region='api',
+		#Switch to write errors to file
 		[Parameter()]
-		[ValidateNotNullOrEmpty()]
-		[ValidatePattern('^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$')]
-		[string]$usr = $Global:verkadaConnection.usr
+		[switch]$errorsToFile
 	)
 	
 	begin {
+		$url = "https://$($region).verkada.com/access/v1/access_users/user/profile_photo"
 		#parameter validation
 		if ([string]::IsNullOrEmpty($org_id)) {throw "org_id is missing but is required!"}
-		if ([string]::IsNullOrEmpty($x_verkada_token)) {throw "x_verkada_token is missing but is required!"}
-		if ([string]::IsNullOrEmpty($x_verkada_auth)) {throw "x_verkada_auth is missing but is required!"}
-		if ([string]::IsNullOrEmpty($usr)) {throw "usr is missing but is required!"}
-
-		$body = @{}
+		if ([string]::IsNullOrEmpty($x_verkada_auth_api)) {throw "x_verkada_auth_api is missing but is required!"}
+		$myErrors = @()
 	} #end begin
 	
 	process {
-		if ([string]::IsNullOrEmpty($userId)){
-			Write-Error "userId required"
+		if ([string]::IsNullOrEmpty($externalId) -and [string]::IsNullOrEmpty($userId)){
+			Write-Error "Either externalId or userId required"
 			return
 		}
-
-		$url = "https://vcerberus.command.verkada.com/user/photos/$org_id/$userId"
-
+		$body_params = @{}
+		
+		$query_params = @{}
+		if (!([string]::IsNullOrEmpty($userId))){
+			$query_params.user_id = $userId
+		} elseif (!([string]::IsNullOrEmpty($externalId))){
+			$query_params.external_id = $externalId
+		}
+		
 		try {
-			Invoke-VerkadaCommandCall $url $org_id $body -x_verkada_token $x_verkada_token -x_verkada_auth $x_verkada_auth -usr $usr -Method 'DELETE'
-			return "Profile pictured removed from user $userId"
+			Invoke-VerkadaRestMethod $url $org_id $x_verkada_auth_api $query_params -body_params $body_params -method 'DELETE'
+			return "Profile pictured removed from user $($query_params | ConvertTo-Json -Compress)"
 		}
 		catch [Microsoft.PowerShell.Commands.HttpResponseException] {
 			$err = $_.ErrorDetails | ConvertFrom-Json
 			$errorMes = $_ | Convertto-Json -WarningAction SilentlyContinue
 			$err | Add-Member -NotePropertyName StatusCode -NotePropertyValue (($errorMes | ConvertFrom-Json -Depth 100 -WarningAction SilentlyContinue).Exception.Response.StatusCode) -Force
-
-			If ($err.StatusCode -eq 500){
-				Write-Host "$($err.StatusCode) - $($err.message) - Most likely because no picture exists." -ForegroundColor Red
-			} else {
-				Write-Host "$($err.StatusCode) - $($err.message)" -ForegroundColor Red
-			}
-			Return
+			$msg = "$($err.StatusCode) - $($err.message)"
+			$msg += ": $(($query_params + $body_params) | ConvertTo-Json -Compress)"
+			Write-Error $msg
+			$myErrors += $msg
+			$msg = $null
 		}
-		
+		catch [VerkadaRestMethodException] {
+			$msg = $_.ToString()
+			$msg += ": $(($query_params + $body_params) | ConvertTo-Json -Compress)"
+			Write-Error $msg
+			$myErrors += $msg
+			$msg = $null
+		}
 	} #end process
 	
 	end {
-		
+		if ($errorsToFile.IsPresent){
+			if (![string]::IsNullOrEmpty($myErrors)){
+				Get-Date | Out-File ./errors.txt -Append
+				$myErrors | Out-File ./errors.txt -Append
+			}
+		}
 	} #end end
 } #end function
